5446dda3edee5761fe6a60f0bf55fda4efe73f6e
michael.aroriz <michael.aroriz@CAS419465W0709.senacsp.edu.br>
2016-05-17 21:32:01 -0300

    Atualização de métodos

--- AgendamentoLocadora/src/Cadastro/CadastroCliente.java
+++ AgendamentoLocadora/src/Cadastro/CadastroCliente.java
@@ -12,11 +12,12 @@ import sun.util.calendar.LocalGregorianCalendar.Date;
  * @author michael.aroriz
  */
 public class CadastroCliente {
+
     //Atributos
     private String nome;
     private String cpf;
     private Date datanascimento;
-    private char sexo;
+    private String sexo;
     private String endereco;
     private String numero;
     private String complemento;
@@ -25,7 +26,7 @@ public class CadastroCliente {
     private String uf;
 
     //Construtor
-    public CadastroCliente(String nome, String cpf, Date datanascimento, char sexo, String endereco, String numero, String complemento, String bairro, String cidade, String uf) {
+    public CadastroCliente(String nome, String cpf, Date datanascimento, String sexo, String endereco, String numero, String complemento, String bairro, String cidade, String uf) {
         this.nome = nome;
         this.cpf = cpf;
         this.datanascimento = datanascimento;
@@ -37,54 +38,99 @@ public class CadastroCliente {
         this.cidade = cidade;
         this.uf = uf;
     }
-    //Métodos
-    public CadastroCliente(String nome) {
-        this.nome = nome;
-    }
-    
+
+    //Métodos     
     public void setNome(String nome) {
         this.nome = nome;
     }
-    public String getNome(){
+
+    public String getNome() {
         return nome;
     }
-    public void imprimirN(){
-        System.out.println(nome);
+
+    public void imprimirN() {
+        System.out.println(sexo);
     }
+
     public void setCpf(String cpf) {
         this.cpf = cpf;
+    }
+
+    public String getCpf() {
+        return cpf;
     }
 
     public void setData(Date data) {
         this.datanascimento = datanascimento;
     }
 
-    public void setSexo(char sexo) {
-        this.sexo = sexo;
+    public Date getData() {
+        return datanascimento;
+    }    
+    
+    public void setSexo(Object sexo) {
+        if (sexo.equals("Masculino")) {
+            sexo = "M";
+            this.sexo = (String) sexo;
+        } else if (sexo.equals("Feminino")) {
+            sexo = "F";
+            this.sexo = (String) sexo;
+        } else {
+            sexo = null;
+            this.sexo = (String) sexo;
+        }
+    }
+    
+    public String getSexo() {
+        return sexo;
     }
 
     public void setEndereco(String endereco) {
         this.endereco = endereco;
     }
 
+    public String getEndereco() {
+        return endereco;
+    }
+
     public void setNumero(String numero) {
         this.numero = numero;
+    }
+
+    public String getNumero() {
+        return numero;
     }
 
     public void setComplemento(String complemento) {
         this.complemento = complemento;
     }
 
+    public String getComplemento() {
+        return complemento;
+    }
+
     public void setBairro(String bairro) {
         this.bairro = bairro;
+    }
+
+    public String getBairro() {
+        return bairro;
     }
 
     public void setCidade(String cidade) {
         this.cidade = cidade;
     }
 
+    public String getCidade() {
+        return cidade;
+    }
+
     public void setUf(String uf) {
         this.uf = uf;
     }
 
+    public String getUf() {
+        return uf;
+    }
+    
 }
