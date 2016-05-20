dc16f7f054eecdfa0a461e099cdb1a801fa88743
sergio <sergio@sergio-Aspire-4739>
2016-05-20 18:35:40 -0300

    Criação do arquivo DAO, início da criação de instruções para inserção no Banco de Dados.

---
+++ AgendamentoLocadora/src/database/ClienteDAO.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database;
+
+import java.sql.Statement;
+
+/**
+ *
+ * @author sergio
+ */
+public class ClienteDAO {
+    
+    private Statement acesso;
+    
+    public void CadastroClientes(Cadastro.CadastroCliente cliente){
+        //Realizar conexão ao BD
+        ConexaoDB.ConnectDB();
+        
+        try {
+            acesso.executeUpdate("insert into CLIENTE(nome,cpf,datadenascimento,sexo,endereco,complemento,bairro,cidade,uf values)'"+cliente.getNome()+"','" +cliente.getCpf()+"','"+cliente.getData()+"','"+cliente.getSexo()+"','"+cliente.getEndereco()+"','"+cliente.getComplemento()+"','"+cliente.getBairro()+"','"+cliente.getCidade()+"','"+cliente.getUf()+"';'");
+        } catch (Exception e) {
+        }
+        
+    }
+    
+    
+    
+}
