678febf4f03af4fc414654cf0a8e4e02de0cc74a
Michaelroriz <michael.advincola@gmail.com>
2016-05-29 21:58:43 -0300

    Atributos adcionados

--- AgendamentoLocadora/src/Agendamento/AgendamentoLocacao.java
+++ AgendamentoLocadora/src/Agendamento/AgendamentoLocacao.java
@@ -5,6 +5,9 @@
  */
 package Agendamento;
 
+import java.sql.Time;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import sun.util.calendar.LocalGregorianCalendar;
 import sun.util.calendar.LocalGregorianCalendar.Date;
 
@@ -16,15 +19,27 @@ public class AgendamentoLocacao {
 
     //Atributos
     
+    private Time horasaida;
     private Date datasaida;
     private Date dataretorno;
+    private Date horaretorno;
+    private String pais;
+    private String cidade;
+    private String local;
 
     //Construtor
-    public AgendamentoLocacao(Date datasaida, Date dataretorno) {
+    public AgendamentoLocacao(Date datasaida, Date dataretorno, Time horasaida, Date horaretorno, String pais, String cidade, String local) {
         this.datasaida = datasaida;
         this.dataretorno = dataretorno;
+        this.horasaida = horasaida;
+        this.horaretorno = horaretorno;
+        this.pais = pais;
+        this.cidade = cidade;
+        this.local = local;
     }
+
     //Métodos
+
     public Date getDatasaida() {
         return datasaida;
     }
@@ -39,5 +54,47 @@ public class AgendamentoLocacao {
 
     public void setDataretorno(Date dataRetorno) {
         this.dataretorno = dataretorno;
+    }
+
+    //////////////////////////////////////////////////////////////
+
+    public void setHorasaida(Time horaSaida) {
+        this.horasaida = horasaida;
+    }
+
+    public Time getHoraSaida() {
+        return horasaida;
+    }
+
+    public void setHoraRetorno(Date horaRetorno) {
+        this.horaretorno = horaretorno;
+    }
+
+    public Date getHoraRetorno() {
+        return horaretorno;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public String getLocal() {
+        return local;
     }
 }
